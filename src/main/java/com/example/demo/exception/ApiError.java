d1394c2375e937e9968358f1da8da5aa99476c28
bala-amypo <bala@amypo.in>
2025-12-23 05:08:54 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiError.java
+++ src/main/java/com/example/demo/exception/ApiError.java
@@ -22,7 +22,7 @@ public class ApiError {
     }
 
     public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+     this.timestamp = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
 
     public int getStatus() { return status; }
     public void setStatus(int status) { this.status = status; }
