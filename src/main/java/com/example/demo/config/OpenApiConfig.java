b92246d28087739f2631635023fbca278806c0a3
bala-amypo <bala@amypo.in>
2025-12-26 11:04:36 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,13 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -15,22 +11,10 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        // JWT Bearer Security Scheme
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
         return new OpenAPI()
-                // Trainer-provided server (DO NOT CHANGE)
+                // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9273.408procr.amypo.ai/")
-                ))
-                // Add JWT support to Swagger
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", bearerAuth))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList("bearerAuth"));
-    }
+                ));
+        }
 }
